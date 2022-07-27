54f7fec078124cdbcb25e6f5feba5f84ec0abfee
ensamuel <ensamuel11@gmail.com>
2022-07-27 21:45:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var students = document.querySelector('#students');
+
+
+var i = 0;
+var student = setInterval(()=>{
+  i++;
+  students.innerText = i;
+
+  if(i>=50){
+    clearInterval(student)
+  };
+
+}, 300);
+
+
+var courses = document.querySelector('#courses');
+var j = 0;
+var course = setInterval(()=>{
+  j++;
+  courses.innerText = j;
+
+  if(j>=10){
+    clearInterval(course)
+  }
+}, 300);
+
+var jobs = document.querySelector('#jobs');
+var k = 0;
+var job = setInterval(()=>{
+  k++;
+  jobs.innerText = k;
+
+  if(k>=100){
+    clearInterval(job)
+  }
+}, 300);
+
